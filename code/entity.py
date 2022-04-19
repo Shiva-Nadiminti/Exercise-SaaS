7554fb5f95ceb1866370321ca9000dd0f8af66b5
Shiva Kumar Nadiminti <100237246+Shiva-Nadiminti@users.noreply.github.com>
2022-04-19 00:03:53 -0700

    Create entity.py

---
+++ code/entity.py
@@ -0,0 +1,9 @@
+import boto3
+fraudDetector = boto3.client('frauddetector')
+
+fraudDetector.put_entity_type(
+   name = 'sample_customer',
+   description = 'sample customer entity type'
+)
+            
+      
